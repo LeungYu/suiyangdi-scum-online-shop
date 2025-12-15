52600c61ecd9e3cdc4936df93ff6da93f8ce86f1
Leung Yu <1065617282@qq.com>
2025-12-16 01:11:17 +0900

    fix

---
+++ main/env-admin.js
@@ -0,0 +1,7 @@
+﻿var prod_env = {
+  "SERVER_NAME": "髓扬帝SCUM商城",
+  "BACKEND_HOST": "http://localhost:10000"
+}
+document.title = `${prod_env.SERVER_NAME}服务器后台`
+window.SERVER_NAME = prod_env.SERVER_NAME
+window.BACKEND_HOST = prod_env.BACKEND_HOST
